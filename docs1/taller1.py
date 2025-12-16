ad008063839dcad8b15f7fe05f2c8b01310d9f7c
zamir david angarita cadavid <zamirdavid242@gmail.com>
2025-12-16 10:17:37 -0500

    zake26

--- docs1/taller1.py
+++ docs1/taller1.py
@@ -5,11 +5,16 @@ numero = int(input("Ingrese un número entero: "))
 if numero > 0:
    
    
+   
+   
+   
    print("El número es positivo")
 
 elif numero < 0:
     print("El número es negativo")
 else:
     print("El número es cero")
+
+
 
 
--- docs1/taller2.py
+++ docs1/taller2.py
@@ -1,2 +1,6 @@
 for i in range(1, 11):
+    
+    
+    
+    
     print(i)
